febb55322395c8de057f66ac45b5dfaa43561eab
lpatmo <linda.peng@alumni.duke.edu>
2018-10-06 11:16:30 -0700

    add news app

--- actionify/urls.py
+++ actionify/urls.py
@@ -19,3 +19,21 @@ from django.urls import path
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
+
+# Use include() to add paths from the catalog application
+from django.conf.urls import include
+
+urlpatterns += [
+    path('news/', include('news.urls')),
+]
+
+from django.views.generic import RedirectView
+urlpatterns += [
+    path('', RedirectView.as_view(url='/news/')),
+]
+
+# Use static() to add url mapping to serve static files during development (only)
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
