7249c10c16c99082aad4384df7736eee65426b00
lpatmo <linda.peng@alumni.duke.edu>
2018-10-06 13:36:18 -0700

    add models to admin

--- news/admin.py
+++ news/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Event, News, Tag, Action, SpamAction, EventAction, EventTag, WatchedEvent, UserVote
+admin.site.register(Event)
+admin.site.register(News)
+admin.site.register(Tag)
+admin.site.register(Action)
+admin.site.register(SpamAction)
+admin.site.register(EventAction)
+admin.site.register(EventTag)
+admin.site.register(WatchedEvent)
+admin.site.register(UserVote)
